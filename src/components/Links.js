0246119c9abe15220540239a87470800e6ad1c1d
Valencia White <valenciawhite@rocketmail.com>
2022-03-16 12:10:41 -0400

    commit tree 60eb0daec6c23faca46059838b60ca98775f879e

---
+++ src/components/Links.js
@@ -0,0 +1,19 @@
+import react from "react";
+
+function Links(props){
+return(
+    <>
+    <h3>Links</h3>
+    <a href={props.github}>{props.github}</a>
+    <a href={props.linkedin}>{props.linkedin}</a>
+    </>
+)
+}
+
+
+
+
+
+
+
+export default Links;
